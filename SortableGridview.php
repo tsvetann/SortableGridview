32058d95e7727c0f3f68570efb3cd5aec6206af3
Tsvetan Nikolov <tsvetan@itstrategen.com>
2014-03-05 13:31:20 +0100

    removed unneded namespace

--- SortableGridview.php
+++ SortableGridview.php
@@ -5,7 +5,12 @@ namespace tsvetann\sortablegridview;
 use yii\grid\GridView;
 use yii\helpers\Json;
 use yii\jui\SortableAsset;
-use yii\web\JsExpression;
+
+/**
+ * Class SortableGridview. A sortable gridview for Yii2
+ * @package tsvetann\sortablegridview
+ * @author Tsvetan Nikolov <live.webscore@gmail.com>
+ */
 
 class SortableGridview extends GridView
 {
